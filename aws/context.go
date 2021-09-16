88940b84441544eb74c0517d2ab057049de6cd3b
Isuru Siriwardana <isuruanatomy@gmail.com>
2021-09-16 11:49:19 +0530

    new session

--- aws/context.go
+++ aws/context.go
@@ -1,6 +1,7 @@
 package aws
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/workdocs"
 )
@@ -9,9 +10,9 @@ import (
 // resources and services.
 // TODO: this code must further optimized.
 func GetAWSSession() *workdocs.WorkDocs {
-	awsSession := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
+	awsSession, _ := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1")},
+	)
 	
 	svc := workdocs.New(awsSession)
 	
